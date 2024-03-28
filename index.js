3bdcbcc7b0e71a011c32d56cddd9dce9530c03a8
HarshitIndira <sharshit@indiratrade.com>
2024-03-28 12:38:36 +0530

    Implemented Basic API- GET,POST,PUT,DELETE

--- index.js
+++ index.js
@@ -5,11 +5,38 @@ const connect = require('./dbconnect');
 //instance creation of express
 const app = express();
 
-//GET api to read the data 
+//Handle data send bt browser in json format
+app.use(express.json());
+
+//GET API to read the data 
 app.get('/',async(req, resp)=>{
     let data = await connect();
     let result =await data.find().toArray();
     resp.send(result);
+});
+
+//PUT API to insert the data 
+app.post('/',async(req, resp)=>{
+    let data = await connect();
+    data = await data.insertOne(req.body);
+    resp.send(data);
+});
+
+//PUT API to update the data
+app.put('/', async (req, resp)=>{
+    let data = await connect();
+    let result = await data.updateOne(
+        {name: req.body.name},
+        {$set: req.body}
+    );
+    resp.send(result);
+});
+
+//DELETE Api to delete the data
+app.delete('/', async(req, resp)=>{
+    let data = await connect();
+    await data.deleteOne(req.body);
+    resp.send("Deleted successfully...");
 });
 
 //creating a port
